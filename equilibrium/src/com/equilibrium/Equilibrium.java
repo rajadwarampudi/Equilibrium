9b930a35eaa8b902b760b7cc1772d9f642a059bc
Raja dwarampudi <rajeshreddy6622@gmail.com>
2016-09-08 18:21:55 +0200

    bug fix and explanatory comments

--- equilibrium/src/com/equilibrium/Equilibrium.java
+++ equilibrium/src/com/equilibrium/Equilibrium.java
@@ -1,12 +1,27 @@
 package com.equilibrium;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Equilibrium {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		        
         Solution sol = new Solution();
-        int A[] = {-1,3,-4,5,1,-6,2,1};
+        
+        int A[] = {1,2,3,4,8,-8,8,4,1,3,2};
         System.out.println(sol.solution(A));
+        
+        int B[] = {1,2,3,4};
+        System.out.println(sol.solution(B));
+        
+        int C[] = {1,2,-1,-2,9};
+        System.out.println(sol.solution(C));
+        
+        int D[] = {9,1,2,-1,-2};
+        System.out.println(sol.solution(D));
+        
+        
 	}
 
 }
--- equilibrium/src/com/equilibrium/Solution.java
+++ equilibrium/src/com/equilibrium/Solution.java
@@ -2,24 +2,30 @@ package com.equilibrium;
 
 public class Solution {
 	public int solution(int[] A) {
-        // write your code in Java SE 8
-		int i = 0;
-		int j = A.length-1;
+		int i = 0;                       //forward index
+		int j = A.length-1;              //reverse index
 		int leftSum = 0,rightSum = 0;
+		
 		while(i < j)
-			leftSum += A[i++];
-		i = 0;
-		while(j>1)
+			leftSum += A[i++];         //calculating the total sum first
+		
+		while(j>=1)
 		{
+			//subtract each number from the last from the total_sum
+			//calculate sum of numbers from the last at the same time
+			//check if the left and right sums are equal at any point of time
+			
 			if(rightSum == leftSum)
 				return j;
 			else{
 				rightSum += A[j];
 				leftSum -= A[j-1];
 				--j;
-			}
-			
-		}
+			}		
+		}//end of while (j >= 1)
+		if(rightSum == leftSum)
+			return j;
+		
 		return -1;
-    }
+    }// end of solution
 }
